49313c05417eda8c4a327fddc07d03e7d04cab52
Tibet <tibet@tools.dev>
2026-03-18 03:25:11 +0000

    Remove all gridfinity files

--- gridfinity_files/config.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
-  window._POSTHOG_REMOTE_CONFIG['phc_vCXdY5oHszJtDAAk2F7bX1oL2FmT7lgV3I23gEkTEfu'] = {
-    config: {"analytics": {"endpoint": "/i/v0/e/"}, "autocaptureExceptions": false, "autocapture_opt_out": false, "captureDeadClicks": false, "capturePerformance": {"network_timing": true, "web_vitals": false, "web_vitals_allowed_metrics": null}, "conversations": false, "defaultIdentifiedOnly": true, "elementsChainAsString": true, "errorTracking": {"autocaptureExceptions": false, "suppressionRules": []}, "hasFeatureFlags": true, "heatmaps": true, "logs": {"captureConsoleLogs": false}, "productTours": false, "sessionRecording": false, "supportedCompression": ["gzip", "gzip-js"], "surveys": false, "token": "phc_vCXdY5oHszJtDAAk2F7bX1oL2FmT7lgV3I23gEkTEfu"},
-    siteApps: []
-  }
-})();
